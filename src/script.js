601aae12afb5a34964299ddd476545815ff1810d
sydneywait <sydneywait@gmail.com>
2019-01-30 14:53:14 -0500

    added functionality to check truthy/falsey if object keys are in file, and pull images from the json to print to dom

--- src/script.js
+++ src/script.js
@@ -12,23 +12,41 @@ fetch("http://localhost:8088/food")
         let foodString = "";
         parsedFoods.forEach(singleFood => {
             console.log(singleFood);
-            
+
 
             fetch(`https://world.openfoodfacts.org/api/v0/product/${singleFood.barcode}.json`)
                 .then(response => response.json())
                 .then(productInfo => {
-                      
-                        let ingredientString = `Ingredients: ${productInfo.product.ingredients_text_en}`
-                        let countryString = `Country of Origin: ${productInfo.product.countries}`
-                        let calorieString =  `Calories: ${productInfo.product.nutriments.energy_value} kCal`
-                        let fatString = `Fat: ${productInfo.product.nutriments.fat} g`
-                        let sugarString =  `Sugar: ${productInfo.product.nutriments.sugars_100g} g/100g`
 
-                        foodString = foodString + `<div class ="food-object"><h2>${singleFood.name}</h2><p>${singleFood.category}</p><p>${singleFood.ethnicity}</p><p>${ingredientString}</p><p>${calorieString}</p><p>${fatString}</p><p>${sugarString}</p><p>${countryString}</p></div>`
+                    let ingredientString = `Ingredients:`
+                    if (productInfo.product.ingredients_text) {
+
+                        ingredientString = `Ingredients: ${productInfo.product.ingredients_text}`
+                    }
+                    else if (productInfo.product.ingredients_text_en) {
+
+                        ingredientString = `Ingredients: ${productInfo.product.ingredients_text_en}`
+                    }
+                     
+                    //checks if there is an object key for the image.  If not, it uses a placeholder
+                    // let imageUrl = "https://zabas.com/wp-content/uploads/2017/01/food-placeholder.png"                    
+                    // if(productInfo.product.image_front_url){
+                    //     imageUrl = `${productInfo.product.image_front_url}`
+                    // }
+
+                    let imageUrl = productInfo.product.image_front_url ? productInfo.product.image_front_url : "https://zabas.com/wp-content/uploads/2017/01/food-placeholder.png"
+
+                        let countryString = `Country of Origin: ${productInfo.product.countries}`
+                        let calorieString = `Calories: ${productInfo.product.nutriments.energy_value} kCal`
+                        let fatString = `Fat: ${productInfo.product.nutriments.fat} g`
+                        let sugarString = `Sugar: ${productInfo.product.nutriments.sugars_100g} g/100g`
+                        
+
+                        foodString = foodString + `<div class ="food-object"><h2>${singleFood.name}</h2><img src = ${imageUrl} alt ="food" width = "100%"><p>${singleFood.category}</p><p>${singleFood.ethnicity}</p><p>${ingredientString}</p><p>${calorieString}</p><p>${fatString}</p><p>${sugarString}</p><p>${countryString}</p></div>`
                         document.querySelector(".foodList").innerHTML = foodString
                     
                 })
-        })
+    })
 
 
 
